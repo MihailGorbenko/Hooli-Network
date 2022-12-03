156f5d9063011080e2a9ce42ba325bb2d41668e1
miha <gomihagle@gmail.com>
2022-12-03 23:29:55 +0200

    restruct + modify server

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
+const path = require('path')
+const config = require('config')
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
 
-const server = express()
+HTTP_PORT = config.get('http_port')
+HTTPS_PORT = config.get('https_port')
 
-server.use(express.static(__dirname))
-server.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'index.html'))
-    console.log('got request');
-})
+const app = express()
+const appSec = express()
 
+app.use(express.static(path.join(__dirname, '/client')))
+app.use('/node_modules', express.static(path.join(__dirname,'/node_modules')))
+appSec.use(express.static(path.join(__dirname, '/client')))
+appSec.use('/node_modules', express.static(path.join(__dirname,'/node_modules')))
 
-server.listen(5000, ()=>console.log('App started.'))
+http.createServer(app).listen(HTTP_PORT, () => {console.log(`Server listening port ${HTTP_PORT}`);})
+
+https.createServer({
+    key: fs.readFileSync('hkeys/key.pem'),
+    cert: fs.readFileSync('hkeys/cert.pem')
+},appSec).listen(HTTPS_PORT, () => {console.log(`Server listening port ${HTTPS_PORT}`);})
 
 
